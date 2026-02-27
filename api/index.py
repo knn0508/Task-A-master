1e00e91f6e3897e815e882db90640e4152b970da
knn0508 <kanan.salmanli.i@student.aztu.edu.az>
2026-02-27 17:52:13 +0400

    Add frontend directory

--- api/index.py
+++ api/index.py
@@ -3,6 +3,7 @@ import sys
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 BACKEND_DIR = os.path.join(BASE_DIR, "backend")
+FRONTEND_DIR = os.path.join(BASE_DIR, "frontend")
 
 if BACKEND_DIR not in sys.path:
     sys.path.insert(0, BACKEND_DIR)
